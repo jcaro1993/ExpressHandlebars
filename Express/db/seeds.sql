78351608a77937e7da938ea56cd31be67d2af62d
Javier Caro <jcaro1993@gmail.com>
2020-12-12 22:22:02 -0800

    edit css

---
+++ Express/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('The Ice Cold Hipster Burger');
+INSERT INTO burgers (name) VALUES ('The Crispy Squirrel Burger');
+INSERT INTO burgers (name, eaten) VALUES ('The Barking Cow Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('The Wild Goat Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('The spicy Pig', true);
+INSERT INTO burgers (name) VALUES ('The Baked Cat Burger');
